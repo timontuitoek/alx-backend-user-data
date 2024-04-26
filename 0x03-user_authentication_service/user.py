2e799338024a848d0d5b1de8bea8e8b044ca8261
timontuitoek <tuitoektimon@gmail.com>
2024-04-26 20:35:28 +0300

    minor correction

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -21,10 +21,10 @@ class User(Base):
     __tablename__ = 'users'
 
     id: int = Column(Integer, primary_key=True)
-    email: str = Column(String, nullable=False)
-    hashed_password: str = Column(String, nullable=False)
-    session_id: str = Column(String, nullable=True)
-    reset_token: str = Column(String, nullable=True)
+    email: str = Column(String(250), nullable=False)
+    hashed_password: str = Column(String(250), nullable=False)
+    session_id: str = Column(String(250), nullable=True)
+    reset_token: str = Column(String(250), nullable=True)
 
     def __repr__(self):
         return f"<User(id={self.id}, email='{self.email}',\
