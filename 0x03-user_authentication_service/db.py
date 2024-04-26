d81808b253bdf5eed240f06ae4c32c2a87a44180
timontuitoek <tuitoektimon@gmail.com>
2024-04-26 21:17:20 +0300

    fixing logging messages

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -58,8 +58,8 @@ class DB:
         try:
             user = self._session.query(User).filter_by(**kwargs).first()
             if user is None:
-                raise NoResultFound(
-                    ("No user found with the specified criteria"))
+                raise NoResultFound\
+                    ("No user found with the specified criteria")
             return user
         except NoResultFound:
             raise  # Re-raise NoResultFound
